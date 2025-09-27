1052f450075e94016aa9446d35f75733086fbff8
ivanprogramm1 <ivanrusnkz@gmail.com>
2025-09-27 20:40:22 +0300

    Delete src/functions.h

--- src/functions.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-int index_first_negative(int arr[], int size);
-int index_last_negative(int arr[], int size);
-int multi_between_negative(int arr[], int size);
-int multi_before_and_after_negative(int arr[], int size);
