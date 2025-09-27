15f90591585684959d6c17e20f47eadb8243a068
ivanprogramm1 <ivanrusnkz@gmail.com>
2025-09-27 20:42:33 +0300

    Update multi_before_and_after_negative.c

--- src/multi_before_and_after_negative.c
+++ src/multi_before_and_after_negative.c
@@ -1,4 +1,6 @@
-#include "functions.h"
+#include "multi_before_and_after_negative.h"
+#include "index_first_negative.h"
+#include "index_last_negative.h"
 
 int multi_before_and_after_negative(int arr[], int size) {
     int first_neg = index_first_negative(arr, size);
