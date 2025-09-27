7c5c00261ab4be09257ebf5417aaea6b3d585802
ivanprogramm1 <ivanrusnkz@gmail.com>
2025-09-27 20:48:42 +0300

    Update menu.c

--- src/menu.c
+++ src/menu.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-#include "functions.h"
-#pragma once
+#include <stdlib.h>
+#include "index_first_negative.h"
+#include "index_last_negative.h"
+#include "multi_between_negative.h"
+#include "multi_before_and_after_negative.h"
 
 int main() {
     int command;
