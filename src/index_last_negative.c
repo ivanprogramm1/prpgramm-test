f86d2f2a8e4ee86d7404095b98ad15a01b0bc739
ivanprogramm1 <ivanrusnkz@gmail.com>
2025-09-27 20:27:02 +0300

    Update index_last_negative.c

--- src/index_last_negative.c
+++ src/index_last_negative.c
@@ -1,17 +1,11 @@
 #include "functions.h"
 
 int index_last_negative(int arr[], int size) {
-    int last_neg = -1;
-    int current_index = 0;
-    
-    while (current_index < size) {
-        int found = index_first_negative(arr + current_index, size - current_index);
-        if (found == -1) {
-            break;
+    int last = -1;
+    for (int i = 0; i < size; i++) {
+        if (arr[i] < 0) {
+            last = i;
         }
-        last_neg = current_index + found;
-        current_index = last_neg + 1;
     }
-    
-    return last_neg;
+    return last;
 }
